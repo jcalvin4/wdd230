941734cad5a2d3de1f9a4e4006a4ef9d800f00fc
jcalvin4 <joe.m.calvin@outlook.com>
2022-05-14 23:53:50 -0600

    chamber home page

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -4,3 +4,4 @@ const mainnav = document.querySelector('.navigation');
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+document.querySelector('.ham').innerHTML = "&#9776"
