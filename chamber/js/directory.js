869b617e7aca8b3d8d2eb1e66f1103ea1413c691
jcalvin4 <97327327+jcalvin4@users.noreply.github.com>
2022-06-18 20:41:10 -0600

    josn

---
+++ chamber/js/directory.js
@@ -0,0 +1,37 @@
+const resultElement = document.querySelector("#business");
+fetch('https://jcalvin4.github.io/wdd230/chamber/json/directory.json')
+.then(response => response.json())
+.then( businesses => {
+    console.log(businesses);
+    
+    businesses = businesses.directory;
+    businesses.forEach( business => {
+    const section = document.createElement('section');
+    
+    const bname = document.createElement('h1');
+    bname.textContent = `${business["business"]}`;
+    
+    const location = document.createElement('h2');
+    location.textContent = `Location: ${business["address"]}`;
+    
+    const contact = document.createElement('h3');
+    contact.textContent = `Phone Number: ${business["phone-number"]}`;
+
+    const email = document.createElement('p');
+    email.textContent =  `Email: ${business["email"]}`;
+
+    const website = document.createElement('p');
+    website.textContent = `Website: ${business["website"]}`;
+    
+    const image = document.createElement('img');
+    image.setAttribute('src', `${business["image"]}`);
+    image.setAttribute('alt', `${business["business"]}`);
+    
+    section.appendChild(bname);
+    section.appendChild(location);
+    section.appendChild(contact);
+    section.appendChild(email);
+    section.appendChild(website);
+    section.appendChild(image);
+    resultElement.appendChild(section);})
+})
